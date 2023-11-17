2676ab719c9f4d83313fa4cc43bb81183a95cc73
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-11-17 03:35:47 +0200

    'update'

--- main/char_to_vec.lua
+++ main/char_to_vec.lua
@@ -1,39 +1,40 @@
 local res = {}
  Ch, res.report,  Render= "始", {},false
- Canvas = love.graphics.setCanvas()
+ Canvas =love.graphics.newCanvas()
 
  
- function intit_canvas()
-    love.graphics.setCanvas(Canvas)
-    love.graphics.clear()
+ 
+ 
+ function  res.get_vec(char)
+    Ch = char
+    intit_canvas()
+    arg_to_canvas()
+    
+    -- read pixels
+    -- scale 
+    -- match to other character
+    -- return report
+    
+end
+function res.render()
+    love.graphics.setCanvas()
+    if Render==true then
+        
+        love.graphics.draw(Canvas,0,0)
+       love.graphics.setCanvas()
+    end
+end
 
-
+function intit_canvas()
+   love.graphics.setCanvas(Canvas)
+   love.graphics.clear()
+   
 end
 
 function arg_to_canvas()
-    love.graphics.print(Ch,30,30)
-    Render= true
+    love.graphics.setBackgroundColor( 0.3,0.4,0.3)
+    print(Ch)
+    love.graphics.print(Ch,300,300)
+    Render = true
 end
-
-    function  res.get_vec(char)
-        Ch = char
-           intit_canvas()
-           arg_to_canvas()
-
--- read pixels
--- scale 
--- match to other character
--- return report
-
-end
-function res.render()
-    print(Render)
-    if Render==true then
-        
-        
-        love.graphics.setCanvas()
-        love.graphics.draw(Canvas,0,0)
-        
-    end
- end
 return res
--- main/main.lua
+++ main/main.lua
@@ -3,22 +3,20 @@ local fontPath = "assets/NotoSerifJP-Black.otf"
 local utf8 = require("utf8")
 local font = love.graphics.newFont(fontPath, 180)
 local matc= require("char_to_vec")
-local char ="働"
+local char ="mlem"
 
 
 function love.load()
     love.graphics.setFont(font)
+    
+  end
+  
+  function love.update(dt)
     matc.get_vec(char)
-       
-end
-
-function love.update(dt)
 
 end
 
 
 function love.draw()
-    -- love.graphics.print(char,300,300)
   matc.render()
-   
 end
